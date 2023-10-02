7edbf6ee46d262f0eb24119cd67d169f50abecf1
Jithan Roy <jithanroyjony@gmail.com>
2023-10-02 21:19:16 +0100

    personal website header done

---
+++ src/containers/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Header } from './';
+
+const App = () => {
+  return (
+    <div className="w-full x:w-[1600px] py-32 px-4 lg:px-12 pr-4 lg:pr-32">
+        {/* particles container */}  
+
+        {/* header */}
+        <Header />
+
+        {/* home container */}
+
+        {/* about container */}
+        {/* skills container */}
+        {/* projects container */}
+        {/* contact container */}
+        {/* footer container */}
+    </div>
+  );
+};
+
+export default App;
